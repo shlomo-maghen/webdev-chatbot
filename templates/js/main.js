ce4a47b7288f105c643e899d198cd914f8763dc0
Rebecca Emmerich <rebeccaemmerich@gmail.com>
2017-01-18 17:46:39 +0200

    Update main.js

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,3 +1,5 @@
-/**
- * Created by DanSamama on 18/01/2017.
- */
+
+setTimeout(function(){document.getElementById("startFirst").style.display='block'}, 1000);
+setTimeout(function(){document.getElementById("startSecond").style.display='block'}, 4000);
+setTimeout(function(){document.getElementById("startThird").style.display='block'}, 7000);
+setTimeout(function(){document.getElementById("startFourth").style.display='block'}, 10000);
