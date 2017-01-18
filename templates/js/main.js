a06d5f4d06eea6e2abb80aabdb9e8a94901ea76c
NHaffner <nataliehaffner@hotmail.com>
2017-01-18 20:30:09 +0200

    add

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,3 +1,23 @@
-/**
- * Created by DanSamama on 18/01/2017.
- */
+function showDiv(number){
+	var items = document.getElementById("mainChat").getElementsByTagName("div");
+	for (var i = 0; i < items.length; i++){
+		items[i].className = (i == number) ? 'shown' : 'hidden';
+	}
+}
+console.log(showDiv);
+
+// var text = ["#startFirst","#startSecond","#startThird","#startFourth"];
+// console.log(text);
+
+// var delay = 400;
+
+// function addOneWord(i, j) {
+//     $('#console_div').append('<br>' + text[i][j]);
+//     if (j+1<text[i].length) {  // next character in the current string
+//         setTimeout(function() { addOneChar(i, j+1); }, delay);
+//     } else if (i+1<text.length) {   // start the next string in the text[] array
+//         setTimeout(function() { addOneChar(i+1, 0); }, delay);
+//     } // else quit
+// }
+
+// setTimeout(function() { addOneChar(0,0); });
